4931992b0bd4086264995328d1dd4d0bbd603ca7
aadilmallick <aadiltj.mallick@gmail.com>
2024-11-17 20:36:53 -0500

    fix some og tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import solidJs from '@astrojs/solid-js';
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), solidJs()]
+  integrations: [tailwind(), react(), solidJs()],
 });
